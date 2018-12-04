df6f76dff102d1ebf5d8289bfda364c3cc133201
Daniel C R <danielcr86@gmail.com>
2018-12-04 23:06:13 +0100

    More tests for chat operations
    Folowing the same principles as previous commit.

--- src/test/java/es/sidelab/webchat/ChatOperationsTest.java
+++ src/test/java/es/sidelab/webchat/ChatOperationsTest.java
@@ -3,6 +3,7 @@ package es.sidelab.webchat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 
 import java.util.Arrays;
@@ -27,13 +28,13 @@ public class ChatOperationsTest {
 		manager = new ChatManager(50);
 	}
 
-
 	@Test
 	public void givenChatManagerWithNUsersRegistered_whenNewChatCreated_thenAllUsersAreNotified() throws InterruptedException, TimeoutException {
 		int numberOfUsers = 2;
 
 		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers);
 
+		// Given
 		User[] spyUsers = new User[numberOfUsers];
 		Arrays.parallelSetAll(spyUsers, i -> {
 			return spy(new TestUser("User " + i) {
@@ -47,14 +48,197 @@ public class ChatOperationsTest {
 
 		Stream.of(spyUsers).forEach(user -> manager.newUser(user));
 
+		// When
 		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
 
 		countDownLatch.await(10, TimeUnit.SECONDS);
 
+		// Then
 		Stream.of(spyUsers).forEach(spy -> {
 			verify(spy, times(1)).newChat(aChat);
 			verify(spy, times(1)).newChat(any());
 		});
-
 	}
+
+	@Test
+	public void givenChatManagerWithNUsersRegisteredAndOneChat_whenChatRemoved_thenAllUsersAreNotified() throws InterruptedException, TimeoutException {
+		int numberOfUsers = 2;
+
+		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers);
+
+		// Given
+		User[] spyUsers = new User[numberOfUsers];
+		Arrays.parallelSetAll(spyUsers, i -> {
+			return spy(new TestUser("User " + i) {
+				@Override
+				public void chatClosed(Chat chat) {
+					countDownLatch.countDown();
+					super.chatClosed(chat);
+				}
+			});
+		});
+
+		Stream.of(spyUsers).forEach(user -> manager.newUser(user));
+		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+
+		// When
+		manager.closeChat(aChat);
+
+		countDownLatch.await(10, TimeUnit.SECONDS);
+
+		// Then
+		Stream.of(spyUsers).forEach(spy -> {
+			verify(spy, times(1)).chatClosed(aChat);
+			verify(spy, times(1)).chatClosed(any());
+		});
+	}
+
+	@Test
+	public void givenChatWithNUsersRegistered_whenNewUserInChat_thenAllUsersAreNotified() throws InterruptedException, TimeoutException {
+		int numberOfUsers = 2;
+
+		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers);
+
+		// Given
+		User[] spyUsers = new User[numberOfUsers];
+		Arrays.parallelSetAll(spyUsers, i -> {
+			return spy(new TestUser("User " + i) {
+				@Override
+				public void newUserInChat(Chat chat, User user) {
+					countDownLatch.countDown();
+					super.newUserInChat(chat, user);
+				}
+			});
+		});
+
+		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+		Stream.of(spyUsers).forEach(user -> aChat.addUser(user));
+
+		// When
+		User newUser = spy(new TestUser("the new user"));
+		aChat.addUser(newUser);
+
+		countDownLatch.await(10, TimeUnit.SECONDS);
+
+		// Then
+		Stream.of(spyUsers).forEach(spy -> {
+			verify(spy, times(1)).newUserInChat(aChat, newUser);
+		});
+
+		verify(newUser, never()).newUserInChat(any(), any());
+	}
+
+	@Test
+	public void givenChatWithNUsersRegistered_whenUserLeavesChat_thenAllUsersAreNotified() throws InterruptedException, TimeoutException {
+		int numberOfUsers = 2;
+
+		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers);
+
+		// Given
+		User[] spyUsers = new User[numberOfUsers];
+		Arrays.parallelSetAll(spyUsers, i -> {
+			return spy(new TestUser("User " + i) {
+				@Override
+				public void userExitedFromChat(Chat chat, User user) {
+					countDownLatch.countDown();
+					super.userExitedFromChat(chat, user);
+				}
+			});
+		});
+
+		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+		Stream.of(spyUsers).forEach(user -> aChat.addUser(user));
+
+		User newUser = spy(new TestUser("the new user"));
+		aChat.addUser(newUser);
+
+		// When
+		aChat.removeUser(newUser);
+
+		countDownLatch.await(10, TimeUnit.SECONDS);
+
+		// Then
+		Stream.of(spyUsers).forEach(spy -> {
+			verify(spy, times(1)).userExitedFromChat(aChat, newUser);
+		});
+
+		verify(newUser, never()).userExitedFromChat(any(), any());
+	}
+
+	@Test
+	public void givenChatWithNUsersRegistered_whenMessageOnTheChat_thenAllUsersGetMessage() throws InterruptedException, TimeoutException {
+		int numberOfUsers = 2;
+
+		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers);
+
+		// Given
+		User[] spyUsers = new User[numberOfUsers];
+		Arrays.parallelSetAll(spyUsers, i -> {
+			return spy(new TestUser("User " + i) {
+				@Override
+				public void newMessage(Chat chat, User user, String message) {
+					countDownLatch.countDown();
+					super.newMessage(chat, user, message);
+				}
+			});
+		});
+
+		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+		Stream.of(spyUsers).forEach(user -> aChat.addUser(user));
+
+		User newUser = spy(new TestUser("the new user"));
+		aChat.addUser(newUser);
+
+		// When
+		aChat.sendMessage(newUser, "message sent by newUser");
+
+		countDownLatch.await(10, TimeUnit.SECONDS);
+
+		// Then
+		Stream.of(spyUsers).forEach(spy -> {
+			verify(spy, times(1)).newMessage(aChat, newUser, "message sent by newUser");
+		});
+	}
+
+	@Test
+	public void givenTwoChatsWithUsersRegisteredOnBoth_whenMessagesOnEachChat_thenTheyGetMessages() throws InterruptedException, TimeoutException {
+		int numberOfUsers= 2;
+
+		CountDownLatch countDownLatch = new CountDownLatch(numberOfUsers * 2);
+
+		// Given
+		User[] spyUsers = new User[numberOfUsers];
+		Arrays.parallelSetAll(spyUsers, i -> {
+			return spy(new TestUser("User " + i) {
+				@Override
+				public void newMessage(Chat chat, User user, String message) {
+					countDownLatch.countDown();
+					super.newMessage(chat, user, message);
+				}
+			});
+		});
+
+		Chat aChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+		Stream.of(spyUsers).forEach(user -> aChat.addUser(user));
+
+		Chat otherChat = manager.newChat("new chat", 5, TimeUnit.SECONDS);
+		Stream.of(spyUsers).forEach(user -> otherChat.addUser(user));
+
+		User newUser = spy(new TestUser("the new user"));
+		aChat.addUser(newUser);
+		otherChat.addUser(newUser);
+
+		// When
+		aChat.sendMessage(newUser, "message sent to a chat");
+		otherChat.sendMessage(newUser, "message sent to the other chat");
+
+		countDownLatch.await(10, TimeUnit.SECONDS);
+
+		// Then
+		Stream.of(spyUsers).forEach(spy -> {
+			verify(spy, times(1)).newMessage(aChat, newUser, "message sent to a chat");
+			verify(spy, times(1)).newMessage(otherChat, newUser, "message sent to the other chat");
+		});
+	}
+
 }
